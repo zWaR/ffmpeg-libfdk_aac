d263ffedbfe6277c3be381fd96f347ffc9c0e821
Ronny Wegener <wegener.ronny@gmail.com>
2013-11-26 18:36:07 +0100

    * changed deb package naming from distribution release to relase

--- build.sh
+++ build.sh
@@ -819,7 +819,7 @@ function build_deb {
 
     if [ "$ENVIRONMENT" == "deb" ]
     then
-        DEBPKG=$CWD/$PKGNAME\_$PKGVERSION\_$(dpkg --print-architecture)_$(cat /etc/*release | grep DISTRIB_ID | cut -d '=' -f 2 | tr '[:upper:]' '[:lower:]')-$(cat /etc/*release | grep DISTRIB_RELEASE | cut -d '=' -f 2).deb
+        DEBPKG=$CWD/$PKGNAME\_$PKGVERSION\_$(dpkg --print-architecture)_$(cat /etc/*release | grep '^ID=' | cut -d '=' -f 2 | tr '[:upper:]' '[:lower:]')-$(cat /etc/*release | grep 'VERSION_ID=' | cut -d '"' -f 2).deb
         mkdir -p $DEB_DIR/DEBIAN
 
         md5sum $(find $BIN_DIR -type f) | sed "s|$BIN_DIR|usr/bin|g" > $DEB_DIR/DEBIAN/md5sums
